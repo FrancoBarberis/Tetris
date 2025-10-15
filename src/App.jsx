0269f5415ffd4aacd90749e9e8562c0ad77ca90d
Franco Barberis <francobarberissic3@gmail.com>
2025-10-15 14:32:00 -0300

    funciones utilitarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import TetrisBoard from './components/TetrisBoard';
-
-const filas = 20;
-const columnas = 10;
-const board = [];
-for (let fila = 0; fila < filas; fila++) {
-  const row = [];
-  for (let columna = 0; columna < columnas; columna++) {
-    row.push(0);
-  }
-  board.push(row);
-}
+import TetrisBoard from "./components/TetrisBoard";
+import { useState } from "react";
+import { usePiece } from "./utils/usePiece";
+import createBoard from "./utils/createBoard";
 
 function App() {
+
+  // DIMENSIONES
+  const filas = 20;
+  const columnas = 10;
+
+  // CREO LA MATRIZ E INICIALIZO EL TABLERO
+  const [board, setBoard] = useState(createBoard(filas, columnas));
+
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-gray-900">
-      <TetrisBoard board={board} />
+      <TetrisBoard board={board}  />
     </div>
   );
 }
