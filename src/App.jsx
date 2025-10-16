35c40dd7aa68fc4e8b24610dbdf465fed17a2b6a
Franco Barberis <francobarberissic3@gmail.com>
2025-10-16 13:48:12 -0300

    Layout Horizontal, styles reseteados

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import TetrisBoard from "./components/TetrisBoard";
-import { useState } from "react";
-import { usePiece } from "./utils/usePiece";
-import createBoard from "./utils/createBoard";
+import Board from "./components/Board";
 
 function App() {
-
-  // DIMENSIONES
-  const filas = 20;
-  const columnas = 10;
-
-  // CREO LA MATRIZ E INICIALIZO EL TABLERO
-  const [board, setBoard] = useState(createBoard(filas, columnas));
-
+  function createMatrix(height, width) {
+    const matrix = [];
+    for (let i = 0; i < height; i++) {
+      matrix.push(new Array(width).fill(0));
+    }
+    return matrix;
+  }
+  /* CRE0 LA MATRIZ QUE RENDERIZARÁ EL TABLERO */
+  const height = 10;
+  const width = 30;
+  const board = createMatrix(height, width);
+  console.log(board);
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-gray-900">
-      <TetrisBoard board={board}  />
-    </div>
+    <>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <h2 className="text-3xl font-medium mb-4">Puntos: <span>{}</span></h2>
+        <Board matrix={board}></Board>
+      </div>
+      {/* CREAR EL LOOP PRINCIPAL DEL JUEGO, ACTUALIZANDO TABLERO CON GRAVITY */}
+      {/* CUANDO UNA PIEZA AL FONDO, SE SOLIDIFICA EN LA MATRIZ */}
+      {/* CUANDO SE SOLIDIFICA UNA PIEZA, SE CREA OTRA PIEZA */}
+      {/* CUANDO SE COMPLETA UNA FILA, SE ELIMINA Y SE BAJA LA MATRIZ COMPLETA */}
+    </>
   );
 }
 
---
+++ src/components/Board.jsx
@@ -0,0 +1,25 @@
+// RESPONSABILIDADES DE BOARD:
+
+// RENDERIZAR EL TABLERO
+//RENDERIZAR LAS PIEZAS SEGÚN LA MATRIZ
+// RENDERIZAR NUEVAMENTE EL TABLERO CUANDO SE HACE UN INPUT
+
+export default function Board({ matrix }) {
+  return (
+    <div
+      className="grid gap-1"
+      style={{ gridTemplateColumns: `repeat(${matrix[0].length}, 1fr)` }}
+    >
+      {matrix.map((fila, y) =>
+        fila.map((celda, x) => (
+          <div
+            key={`${y}-${x}`}
+            className={`w-9 h-9 border ${
+              celda ? "bg-blue-500" : "bg-gray-800"
+            }`}
+          />
+        ))
+      )}
+    </div>
+  );
+}
