c7f6b6826c4301c1a04d76efdd066e1954887ba6
Franco Barberis <francobarberissic3@gmail.com>
2025-10-27 22:50:20 -0300

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import React from "react";
 import Board from "./components/Board";
+// import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
-  return (
-    <>
-      <Board/>
-    </>
-  );
+  // Si quieres pantalla de carga, descomenta abajo y ajusta el estado
+  // const [loading, setLoading] = React.useState(true);
+  // React.useEffect(() => {
+  //   const timer = setTimeout(() => setLoading(false), 2000);
+  //   return () => clearTimeout(timer);
+  // }, []);
+  // return loading ? <LoadingScreen /> : <Board />;
+  return <Board />;
 }
 
 export default App;
